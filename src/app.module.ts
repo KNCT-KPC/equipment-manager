e244c3819b8f445b947b4473fcf9de5f30f09948
Arumino <git@arumino-om.net>
2025-02-20 22:52:20 +0900

    chore: create basic directory structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './modules/user/user.controller';
+import { UserModule } from './modules/user/user.module';
+import { EquipmentModule } from './modules/equipment/equipment.module';
+import { EquipmentController } from './modules/equipment/equipment.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UserModule, EquipmentModule],
+  controllers: [AppController, UserController, EquipmentController],
   providers: [AppService],
 })
 export class AppModule {}
