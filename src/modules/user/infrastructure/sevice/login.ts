6380a4a1bcc32d048466ef41323ddeda821cc68c
as1-2-bidama <sa080201@icloud.com>
2025-02-27 21:03:48 +0900

    ログインの実装

---
+++ src/modules/user/infrastructure/sevice/login.ts
@@ -0,0 +1,19 @@
+import { auth, emailAuthProvider } from "./firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+
+const Sigin = () => {
+signInWithEmailAndPassword(auth,email,password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+    return user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}
+
+export { Sigin };
