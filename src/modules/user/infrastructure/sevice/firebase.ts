23ff4ef98b52b3549b72c9da7761f24eb362c693
as1-2-bidama <sa080201@icloud.com>
2025-03-03 21:14:40 +0900

    config.tsを.envに変更

--- src/modules/user/infrastructure/sevice/firebase.ts
+++ src/modules/user/infrastructure/sevice/firebase.ts
@@ -1,7 +1,14 @@
 import * as admin from 'firebase-admin';
-import { firebaseConfig } from './config';
 
-admin.initializeApp(firebaseConfig);
+const firebaseconfig = {
+  apikey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+}
+admin.initializeApp();
 const auth = admin.auth();
 
 export { auth };
