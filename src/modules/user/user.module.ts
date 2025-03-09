67f0b62349a2189b19696b84dc0c831bde0f344d
as1-2-bidama <sa080201@icloud.com>
2025-03-09 14:07:25 +0900

    Userテーブル操作用関数

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationIDTokenService } from './infrastructure/service/login.service';
-import { FirebaseService } from 'src/infrastructure/firebase/firebase.service';
+import { FirebaseService } from '../../infrastructure/firebase/firebase.service';
+import { UserRepository } from './infrastructure/repositories/user.repository';
 
 @Module({
   controllers: [],
-  providers: [AuthenticationIDTokenService, FirebaseService],
+  providers: [AuthenticationIDTokenService, FirebaseService, UserRepository],
 })
 export class UserModule {}
