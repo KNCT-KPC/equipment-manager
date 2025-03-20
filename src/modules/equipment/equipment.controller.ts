a5f71c6da295159887980972c836cb4eb856adbf
雨宮 <161292141+787B-R26B@users.noreply.github.com>
2025-03-20 10:17:56 +0900

    add: findAll, findById

--- src/modules/equipment/equipment.controller.ts
+++ src/modules/equipment/equipment.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { EquipmentService } from './application/services/equipment.service';
 
 @Controller('equipment')
-export class EquipmentController {}
+export class EquipmentController {
+    constructor(private equipmentService: EquipmentService) {}
+
+    @Get()
+    async getList(
+        @Query('page') page: string = '1',
+        @Query('limit') limit: string = '10'
+    ){
+        return this.equipmentService.getEquipmentList(Number(page), Number(limit))
+    }
+
+    @Get(':id')
+    async getById(@Param('id') id:string) {
+        return this.equipmentService.getEquipmentById(id)
+    }
+}
