72823a377b4d7e114d3ee57f5dd3055fdb1fd5df
雨宮 <161292141+787B-R26B@users.noreply.github.com>
2025-03-20 14:47:03 +0900

    add: error responce, query parameter validation

--- src/modules/equipment/application/services/equipment.service.ts
+++ src/modules/equipment/application/services/equipment.service.ts
@@ -1,16 +1,35 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { EquipmentRepository } from "../../infrastructure/repositories/equipment.repository";
+import { NotFoundError } from "rxjs";
 
 @Injectable()
 export class EquipmentService {
-    constructor(private equipmentRepo: EquipmentRepository) {}
+    constructor(private readonly equipmentRepo: EquipmentRepository) {}
 
-    async getEquipmentList(page: number = 1, limit: number = 10) {
-        const skip = (page -1) * limit
-        return this.equipmentRepo.findAll(skip, limit)
+    async findAll(page: number, limit: number){
+        const skip = (page - 1) * limit
+        const items = await this.equipmentRepo.findAll(skip, limit)
+
+        if (!items || items.length === 0 ){
+            throw new NotFoundException({
+                statusCode: 404,
+                message: 'No equipment found',
+                error: 'Not found',
+            })
+        }
+        return items
     }
 
     async getEquipmentById(id: string) {
-        return this.equipmentRepo.findById(id)
+        const item = await this.equipmentRepo.findById(id)
+
+        if (!item){
+            throw new NotFoundException({
+                statusCode: 404,
+                message: `Equipment with ID ${id} not found`,
+                error: 'Not found',
+            })
+        }
+        return item
     }
 }
--- src/modules/equipment/equipment.controller.ts
+++ src/modules/equipment/equipment.controller.ts
@@ -1,16 +1,40 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, Query, ValidationPipe } from '@nestjs/common';
 import { EquipmentService } from './application/services/equipment.service';
+import { IsInt, Min, Max, IsPositive } from 'class-validator'
+import { Type } from 'class-transformer'
+import { query } from 'express';
+import { STATUS_CODES } from 'http';
+
+class EquipmentQueryDto{
+    @Type(() => Number)
+    @IsInt({ message: 'page must be a natural number'})
+    @IsPositive({ message: 'page must be a natural number'})
+    page: number
+
+    @Type(() => Number)
+    @IsInt({ message: 'limit must be an integer between 1 and 100'})
+    @Min(1, { message: 'limit must be at last 1'})
+    @Max(100, { message: 'limit must be at most 100'})
+    limit: number
+}
 
 @Controller('equipment')
 export class EquipmentController {
     constructor(private equipmentService: EquipmentService) {}
 
     @Get()
-    async getList(
-        @Query('page') page: string = '1',
-        @Query('limit') limit: string = '10'
-    ){
-        return this.equipmentService.getEquipmentList(Number(page), Number(limit))
+    async getList(@Query(new ValidationPipe({ transform: true})) query: EquipmentQueryDto) {
+        const { page, limit } = query
+        const items = await this.equipmentService.findAll(page, limit)
+
+        if (!items || items.length === 0){
+            throw new NotFoundException({
+                statusCode: 404,
+                message: 'No equipment found',
+                error: 'Not Found',
+            })
+        }
+        return items
     }
 
     @Get(':id')
