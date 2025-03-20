83ed732993e5688daaa91583a5a7718235625b5b
雨宮 <161292141+787B-R26B@users.noreply.github.com>
2025-03-20 10:22:37 +0900

    fix: AppModule

--- src/modules/equipment/equipment.module.ts
+++ src/modules/equipment/equipment.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EquipmentController } from './equipment.controller';
+import { EquipmentService } from './application/services/equipment.service';
 import { EquipmentRepository } from './infrastructure/repositories/equipment.repository';
 import { EquipmentUserRepository } from './infrastructure/repositories/equipmentUser.repository';
 
 @Module({
   controllers: [EquipmentController],
-  providers: [EquipmentRepository, EquipmentUserRepository],
+  providers: [EquipmentService, EquipmentRepository, EquipmentUserRepository],
 })
 export class EquipmentModule {}
