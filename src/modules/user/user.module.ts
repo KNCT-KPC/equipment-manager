f8fa1c2dbbaae443e7d344ba4bc3926bda83134a
雨宮 <161292141+787B-R26B@users.noreply.github.com>
2025-03-20 10:46:12 +0900

    add: import PrismaModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthenticationIDTokenService } from './infrastructure/service/login.service';
 import { FirebaseService } from '../../infrastructure/firebase/firebase.service';
 import { UserRepository } from './infrastructure/repositories/user.repository';
+import { PrismaModule } from 'src/infrastructure/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [],
   providers: [AuthenticationIDTokenService, FirebaseService, UserRepository],
 })
