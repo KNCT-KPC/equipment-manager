3f7634d879f98ef4245e1abdbbb1102c79b473e8
as1-2-bidama <sa080201@icloud.com>
2025-03-30 12:44:59 +0900

    paramの参照を追加

--- src/modules/equipment/application/controller/getEquipmentInfo.controller.ts
+++ src/modules/equipment/application/controller/getEquipmentInfo.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Get } from "@nestjs/common";
-import { EquipmentService } from "../services/equipment.service";
-import { GetEquipmentInfoDto } from "../dto/getEquipmentInfo.dto";
+import { Body, Controller, Get, Param } from '@nestjs/common';
+import { EquipmentService } from '../services/equipment.service';
+import { GetEquipmentInfoDto } from '../dto/getEquipmentInfo.dto';
 
 @Controller('equipment')
 export class EquipmentController {
   constructor(private readonly equipmentService: EquipmentService) {}
 
   @Get()
-  async GetEquipmentInfoID(@Body() GetEquipmentInfoDto : GetEquipmentInfoDto) {
-    const data = await this.equipmentService.getEquipmentById(GetEquipmentInfoDto.id);
-    return data.id, data.name, data.description, data.amount, data.created_at, data.updated_at;
+  async GetEquipmentInfoID(@Body() @Param('param') param: GetEquipmentInfoDto) {
+    const data = await this.equipmentService.getEquipmentById(param.id);
+    return (
+      data.id,
+      data.name,
+      data.description,
+      data.amount,
+      data.created_at,
+      data.updated_at
+    );
   }
 }
