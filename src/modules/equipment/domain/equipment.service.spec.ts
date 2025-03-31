fe7c6ee60676b5386f24e980a2b8fe67e284f700
as1-2-bidama <sa080201@icloud.com>
2025-04-01 00:48:07 +0900

    all chenge test

--- src/modules/equipment/domain/equipment.service.spec.ts
+++ src/modules/equipment/domain/equipment.service.spec.ts
@@ -1,13 +1,12 @@
-import { EquipmentRepository } from "../infrastructure/repositories/equipment.repository";
-import { EquipmentService } from "./service/equipment.service";
-import { PrismaService } from "../../../infrastructure/prisma/prisma.service";
+import { EquipmentRepository } from '../infrastructure/repositories/equipment.repository';
+import { EquipmentService } from './service/equipment.service';
+import { PrismaService } from '../../../infrastructure/prisma/prisma.service';
 
-
-describe('get equipmentlist',()=>{
-  it('物品リスト取得',async ()=>{
+describe('get equipmentlist', () => {
+  it('物品リスト取得', async () => {
     const prisma = new PrismaService();
     const equipmentRepository = new EquipmentRepository(prisma);
     const getequipment = new EquipmentService(equipmentRepository);
-    expect(await getequipment.GetEquipmentList(5,1))
-  })
-})
+    expect(await getequipment.GetEquipmentList(5, 1));
+  });
+});
