4499c23cdae5a2b4a1765212d88489d1082a0c72
as1-2-bidama <sa080201@icloud.com>
2025-03-31 22:48:51 +0900

    関数名変更

--- src/modules/equipment/domain/service/equipment.service.ts
+++ src/modules/equipment/domain/service/equipment.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { EquipmentRepository } from "../../infrastructure/repositories/equipment.repository";
 
 Injectable();
-export class GetEquipmentListService{
+export class GetEquipmentService{
   constructor(private equipment : EquipmentRepository){
     this.equipment = equipment
   }
