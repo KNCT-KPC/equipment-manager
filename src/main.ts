12c83937522417c8477077a52863e976cf4b9b68
Arumino <git@arumino-om.net>
2025-06-22 21:26:01 +0900

    fix: 環境変数名の更新忘れを修正

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.ACCESE_URL,
+    origin: process.env.CORS_ALLOW_ORIGIN,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
